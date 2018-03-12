7c204136a39dae4c45392d536d776204f293a36f
DarknessFire <1160264548@qq.com>
2018-03-12 15:37:29 +0800

    test commit

--- src/main/java/com/myproject/controller/TestController.java
+++ src/main/java/com/myproject/controller/TestController.java
@@ -1,15 +1,23 @@
 package com.myproject.controller;
 
+import com.myproject.dao.BaseDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Map;
+
 @Controller
 @RequestMapping("/")
 public class TestController {
+    @Autowired
+    private BaseDao dao;
     @ResponseBody
     @RequestMapping("/test")
     public String test(){
+        Map<String,Object> map = (Map<String, Object>) dao.findById("1");
+        System.out.print(map.get("a_id"));
         return "Hello world";
     }
 }
---
+++ src/main/java/com/myproject/model/Test.java
@@ -0,0 +1,16 @@
+package com.myproject.model;
+
+public class Test {
+    public String id;
+
+    public Test() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
