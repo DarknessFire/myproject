d1b4cdd1302b9791d8adbdc6ab172cb9f3002221
DarknessFire <1160264548@qq.com>
2018-03-14 10:09:47 +0800

    support mybatis and pagehelper

---
+++ src/main/java/com/myproject/manager/testManager.java
@@ -0,0 +1,8 @@
+package com.myproject.manager;
+
+import com.myproject.dao.impl.BaseMyBatisDaoImpl;
+import org.springframework.stereotype.Service;
+
+@Service
+public class testManager extends BaseMyBatisDaoImpl {
+}
